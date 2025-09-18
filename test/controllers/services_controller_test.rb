43834f6572f109e14f1a0ac482a43977fca91c4b
dezmond55 <derek.james@jbps.com.au>
2025-09-18 23:09:58 +0930

    Update services controller tests with correct routes and methods

--- test/controllers/services_controller_test.rb
+++ test/controllers/services_controller_test.rb
@@ -2,32 +2,41 @@ require "test_helper"
 
 class ServicesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get services_index_url
+    get services_path
     assert_response :success
   end
 
   test "should get new" do
-    get services_new_url
+    get new_service_path
     assert_response :success
   end
 
-  test "should get create" do
-    get services_create_url
-    assert_response :success
+  test "should create service" do
+    assert_difference("Service.count") do
+      post services_path, params: { service: { name: "Test", description: "Test description" } }
+    end
+    assert_redirected_to services_path
+    assert_equal "Service created successfully.", flash[:notice]
   end
 
   test "should get edit" do
-    get services_edit_url
+    service = Service.create(name: "Test", description: "Test description")
+    get edit_service_path(service)
     assert_response :success
   end
 
   test "should get update" do
-    get services_update_url
-    assert_response :success
+    service = Service.create(name: "Test", description: "Test description")
+    patch service_path(service), params: { service: { name: "Updated" } }
+    assert_redirected_to services_path
+    assert_equal "Updated", Service.find(service.id).name
   end
 
   test "should get destroy" do
-    get services_destroy_url
-    assert_response :success
+    service = Service.create(name: "Test", description: "Test description")
+    assert_difference("Service.count", -1) do
+      delete service_path(service)
+    end
+    assert_redirected_to services_path
   end
 end
