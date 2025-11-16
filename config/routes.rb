fc865ce7df8d98750a3d6373c0b42f609f83e069
dezmond55 <derek.james@jbps.com.au>
2025-11-16 22:12:51 +1030

    Hide Services page on live site until ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  resources :services
   root "pages#home"
-  get "up" => "rails/health#show", as: :rails_health_check
-  get "/about" => "pages#about"
-  get "/nodeapp", to: redirect("/")
-  # Optional PWA routes
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  # Remove or comment out
-  # get "pages/home"
+  get "about", to: "pages#about"
+
+  # Hide Services on live site until ready
+  get "/services", to: redirect("/"), unless: -> { Rails.env.development? }
+  get "/services/*path", to: redirect("/"), unless: -> { Rails.env.development? }
+
+  resources :services
 end
